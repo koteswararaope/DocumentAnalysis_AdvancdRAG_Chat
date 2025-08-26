e43034595776d7f099f79730b30e4fb932846377
koteswararaope <koteswararao.pentyala@gmail.com>
2025-08-26 09:48:26 +0530

    doc ingenstion skeliton

---
+++ src/doc_analyzer/dataingestion.py
@@ -0,0 +1,19 @@
+import os
+import fitz
+import uuid
+from datetime import datetime
+from logger.custom_struct_logger import CustomStructLogger
+from exception.custom_exception import DocumentPortalException
+
+class DocumentHandler:
+    """Handles PDF save and read operations and write logs for all actions
+    """
+    
+    def __init__(self):
+        pass
+    
+    def Savepdf(self):
+        pass
+    
+    def ReadPdf(self):
+        pass

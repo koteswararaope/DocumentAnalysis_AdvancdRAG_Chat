cc0b0db2b92fa1f2f7449d34abf5da617436bbb8
koteswararaope <koteswararao.pentyala@gmail.com>
2025-09-01 11:02:46 +0530

    Skelliton for Multidoc Chat

---
+++ src/multidocchat/data_ingestion.py
@@ -0,0 +1,14 @@
+import sys
+from logger.custom_struct_logger import CustomStructLogger
+from exception.custom_exception import DocumentPortalException
+from utils.model_loader import Modelloader
+
+
+class multidocIngestor:
+    def __init__(self):
+        pass
+    def add_docs(self):
+        pass
+    def __create_retriver(self, documents):
+        pass
+    
---
+++ src/multidocchat/retrieval.py
@@ -0,0 +1,19 @@
+
+import sys
+from logger.custom_struct_logger import CustomStructLogger
+from exception.custom_exception import DocumentPortalException
+from utils.model_loader import Modelloader
+
+class ConversationalRAG:
+    def __init__(self):
+        pass 
+    def load_retriever_faiss(self):
+        pass
+    def invoke(self):
+        pass
+    def _load_llm(self):
+        pass
+    def _format_docs(self, docs):
+        pass
+    def _build_lcel_chain(self):
+        pass

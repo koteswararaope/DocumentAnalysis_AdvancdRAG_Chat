11a7288681a42780aeff80f5b6063c5d125ca830
koteswararaope <koteswararao.pentyala@gmail.com>
2025-09-17 15:34:20 +0530

    updatd test case

--- test/unit_test.py
+++ test/unit_test.py
@@ -6,24 +6,37 @@ import io, os
 from pathlib import Path
 client = TestClient(app)
 
+
 def test_home():
     response = client.get("/")
     assert response.status_code == 200
     assert "Document Portal" in response.text
-    
+from pypdf import PdfReader
+
 def test_analysis():
       # Prepare a fake PDF file content
-    file_path = r"C:\Learning\Python\LLM_ops\EndtoEnd\DocumentAnalysis_AdvancdRAG_Chat\data\document_analysis\NIPS-2017-attention-is-all-you-need-Paper.pdf"
-    #file_path = Path(__file__).parent / "data" / "document_analysis" / "NIPS-2017-attention-is-all-you-need-Paper.pdf"
+    file_path = r"data\document_analysis\NIPS-2017-attention-is-all-you-need-Paper.pdf"
+    
     with open(file_path, "rb") as file:
         files = {
             "file": ("NIPS-2017-attention-is-all-you-need-Paper.pdf", file, "application/pdf"),
         }
-
+        
         response = client.post("/analyze", files=files)
-
-    assert response.status_code == 200
+        assert response.status_code == 200
 
     json_response = response.json()
     assert json_response["filename"] == "NIPS-2017-attention-is-all-you-need-Paper.pdf"
-    
+
+
+'''def test_analysis11():
+      # Prepare a fake PDF file content
+    file_path = r"data\document_analysis\NIPS-2017-attention-is-all-you-need-Paper.pdf"
+    reader = PdfReader(file_path)
+    full_text = ""
+
+    for page in reader.pages:
+        text = page.extract_text()
+        if text:
+            full_text += text + "\n"'''
+

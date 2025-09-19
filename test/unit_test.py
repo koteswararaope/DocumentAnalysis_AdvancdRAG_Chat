de3cfcc513bd564ad122d32e14f2271a5b86ea68
koteswararaope <koteswararao.pentyala@gmail.com>
2025-09-19 11:04:54 +0530

    updated test case with fixture for environrmnt varibles

--- test/unit_test.py
+++ test/unit_test.py
@@ -13,12 +13,25 @@ def test_home():
     assert "Document Portal" in response.text
 from pypdf import PdfReader
 
+# Fixture to set and clean up environment variables
+@pytest.fixture(autouse=True)
+def set_and_cleanup_env(monkeypatch):
+    # Setup: set env vars
+    monkeypatch.setenv("GROQ_API_KEY", "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")
+    monkeypatch.setenv("GOOGLE_API_KEY", "AIzaSyCXxhCtUOLdVsN-1uEvVulsSCNxGNEVkyw")
 
-def test_analyze_documents_with_real_pdf():
+    # Yield control back to the test
+    yield
+
+    # Teardown: remove env vars
+    monkeypatch.delenv("GROQ_API_KEY", raising=False)
+    monkeypatch.delenv("GOOGLE_API_KEY", raising=False)
+    
+def test_analyze_documents_with_real_pdf(monkeypatch):
     file_path = Path(__file__).parent / "Sample.pdf"
     
     assert file_path.exists(), f"Test file not found: {file_path}"
-
+    
     with open(file_path, "rb") as f:
         response = client.post(
             "/analyze",
@@ -26,6 +39,7 @@ def test_analyze_documents_with_real_pdf():
         )
         assert response.status_code == 200
         assert "Document Portal" in response.text
+    
 
 
 
